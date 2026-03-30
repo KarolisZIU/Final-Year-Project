ac9a5443d6f29fa0a69f3e361251e293af0cff8f
KarolisZIU <karolinho3216@gmail.com>
2026-03-30 18:49:40 +0100

    Deployed database to azure and tested functionality

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,22 @@
-import pg from "pg";
-import dotenv from "dotenv";
+import pkg from 'pg';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
+
+
+export const pool = new pkg.Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
+
+// test
+const res = await pool.query('SELECT NOW()');
+console.log(res.rows);
